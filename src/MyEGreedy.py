c4e586d81af5738b51273be0611e794d9e9d1961
Marcin Jarosz <m.w.jarosz@student.tudelft.nl>
2023-03-27 07:48:57 +0200

    working impl

--- src/MyEGreedy.py
+++ src/MyEGreedy.py
@@ -1,15 +1,47 @@
+import numpy as np
+
 class MyEGreedy:
     def __init__(self):
         print("Made EGreedy")
 
     def get_random_action(self, agent, maze):
-        # TODO to select an action at random in State s
-        return None
+        """
+        Selects a random action for the agent in some state in the maze
+        :param agent
+        :param maze
+        :return: random action
+        """
+        actions = maze.get_valid_actions(agent)
+        return np.random.choice(actions)
 
     def get_best_action(self, agent, maze, q_learning):
-        # TODO to select the best possible action currently known in State s.
-        return None
+        """
+        Selects the best possible action for the agent in some state in the maze
+        :param agent
+        :param maze
+        :param q_learning
+        :return: best action
+        """
+        s = agent.get_state(maze)
+        actions = maze.get_valid_actions(agent)
+        values = q_learning.get_action_values(s, actions)
+        max_rewards = np.where(values == np.max(values))[0]
+        # If there are multiple best actions, select a random one
+        chosen_index = np.random.choice(max_rewards)
+        return actions[chosen_index]
 
     def get_egreedy_action(self, agent, maze, q_learning, epsilon):
-        # TODO to select between random or best action selection based on epsilon.
-        return None
+        """
+        Selects the best action with probability (1 - epsilon) for the agent;
+        random action with probability (epsilon)
+        :param agent:
+        :param maze:
+        :param q_learning:
+        :param epsilon:
+        :return:
+        """
+        rand = np.random.uniform()
+        if rand < epsilon:
+            return self.get_random_action(agent, maze)
+        else:
+            return self.get_best_action(agent, maze, q_learning)
--- src/MyQLearning.py
+++ src/MyQLearning.py
@@ -1,6 +1,20 @@
 from QLearning import QLearning
 
+
 class MyQLearning(QLearning):
     def update_q(self, state, action, r, state_next, possible_actions, alfa, gamma):
-        # TODO Auto-generated method stub
-        return
+        """
+        :param state: current state
+        :param action: action taken
+        :param r: reward for taking the action
+        :param state_next: the next state after performing action
+        :param possible_actions: all possible actions in state_next
+        :param alfa: learning rate
+        :param gamma: discount
+        """
+        # Calculate the maximum Q-value for the next state
+        max_q_next = max([self.get_q(state_next, a) for a in possible_actions])
+        # Calculate the updated Q value
+        q_updated = (1 - alfa) * self.get_q(state, action) + alfa * (r + gamma * max_q_next)
+        # Update the Q table with the new Q value
+        self.set_q(state, action, q_updated)
